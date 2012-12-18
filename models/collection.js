37b69b82874052d21498ad0976a196292b2a1f58
Andrei Petre <changing.the.habit@gmail.com>
2012-12-18 23:20:09 +0000

    Updated collection model.

--- models/collection.js
+++ models/collection.js
@@ -1,34 +1,40 @@
-var mongoose = require('mongoose');
+var mongoose = require('mongoose'),
+	date = require('../utils/date.js');
 
 
 /**
  * Schema for Collection model.
  */
-var CollectionSchema = new mongoose.Schema(
-	{
-		// fields
-		name: {
-			type: String,
-			required: true,
-			index: true,
-			unique: true,
-			trim: true
-		},
-		author: {
-			type: String,
-			required: false,
-			trim: true
-		},
-		created_on: {
-			type: Date,
-			'default': Date.now,
-			required: true
-		}
+var CollectionSchema = new mongoose.Schema({
+	// fields
+	name: {
+		type: String,
+		required: true,
+		index: true,
+		unique: true,
+		trim: true
 	},
-	{
-		// other options
+	author: {
+		type: String,
+		required: false,
+		trim: true
+	},
+	created_on: {
+		type: Date,
+		'default': Date.now,
+		required: true
 	}
-);
+});
+
+CollectionSchema.virtual('jsonFields').get(function () {
+	return {
+		id: this._id,
+		name: this.name,
+		author: this.author,
+		created_on: date.date2string(this.created_on)
+	};
+});
+
 
 /**
  * Collection model.
