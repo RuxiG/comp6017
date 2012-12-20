d5d04f1355a83f1967c171f5b005fff75a869811
Andrei Petre <changing.the.habit@gmail.com>
2012-12-20 01:26:43 +0000

    Created CommentForm.

--- forms.js
+++ forms.js
@@ -1,8 +1,6 @@
 var forms = require('forms'),
 	fields = forms.fields,
-	validators = forms.validators,
-	
-	models = require('./models.js');
+	validators = forms.validators;
 
 
 /**
@@ -21,6 +19,22 @@ var CollectionForm = forms.create({
 
 
 /**
+ * Form for Comment model.
+ */
+var CommentForm = forms.create({
+	author: fields.string({
+		required: true,
+		validators: [validators.minlength(3)]
+	}),
+	comment: fields.string({
+		required: true,
+		validators: [validators.minlength(3)]
+	})
+});
+
+
+/**
  * Exports.
  */
 exports.CollectionForm = CollectionForm;
+exports.CommentForm = CommentForm;
