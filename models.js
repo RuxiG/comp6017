b29873ad69bd5c366c926f9cd2cda449286dece3
Andrei Petre <changing.the.habit@gmail.com>
2012-12-20 01:25:47 +0000

    Created Comment model and added it to Collection.

--- models.js
+++ models.js
@@ -23,6 +23,10 @@ var CollectionSchema = new mongoose.Schema({
 		type: Date,
 		'default': Date.now,
 		required: true
+	},
+	comments: {
+		type: [{type: mongoose.Schema.Types.ObjectId, ref: 'Comment'}],
+		required: false
 	}
 });
 
@@ -32,11 +36,17 @@ var CollectionSchema = new mongoose.Schema({
  * @returns Object.
  */
 CollectionSchema.virtual('jsonFields').get(function () {
+	// replace list of ObjectIds with actual Comment instances
+	this.populate('comments');
+	
 	return {
 		id: this._id,
 		name: this.name,
 		author: this.author,
-		created_on: utils.date2string(this.created_on)
+		created_on: utils.date2string(this.created_on),
+		comments: this.comments.map(function (comment) {
+			return comment.jsonFields;
+		})
 	};
 });
 
@@ -46,10 +56,16 @@ CollectionSchema.virtual('jsonFields').get(function () {
  * @returns Object.
  */
 CollectionSchema.virtual('jsonFieldsEdit').get(function () {
+	// replace list of ObjectIds with actual Comment instances
+	this.populate('comments');
+	
 	return {
 		id: this._id,
 		name: this.name,
-		author: this.author
+		author: this.author,
+		comments: this.comments.map(function (comment) {
+			return comment.jsonFieldsEdit;
+		})
 	};
 });
 
@@ -81,6 +97,82 @@ var Collection = mongoose.model('Collection', CollectionSchema);
 
 
 /**
+ * Schema for Comment model.
+ */
+var CommentSchema = new mongoose.Schema({
+	author: {
+		type: String,
+		required: true,
+		trim: true
+	},
+	comment: {
+		type: String,
+		required: true,
+		trim: true
+	},
+	created_on: {
+		type: Date,
+		'default': Date.now,
+		required: true
+	}
+});
+
+/**
+ * JSON representation of all fields (that the user can see).
+ * 
+ * @returns Object.
+ */
+CommentSchema.virtual('jsonFields').get(function () {
+	return {
+		id: this._id,
+		author: this.author,
+		comment: this.comment,
+		created_on: utils.date2string(this.created_on)
+	};
+});
+
+/**
+ * JSON representation of all fields that can be edited.
+ * 
+ * @returns Object.
+ */
+CommentSchema.virtual('jsonFieldsEdit').get(function () {
+	return {
+		id: this._id,
+		author: this.author,
+		comment: this.comment
+	};
+});
+
+/**
+ * Create a Comment instance from the form's data.
+ * 
+ * @param CommentForm form.
+ * @returns Comment or null if form has errors;
+ * does not save the instance to the DB.
+ */
+CommentSchema.statics.createFromForm = function (form) {
+	if (!form.isValid()) {
+		return null;
+	}
+	
+	var comment = this.model('Comment')({
+		author: form.data.author,
+		comment: form.data.comment
+	});
+	
+	return comment;
+};
+
+
+/**
+ * Comment model.
+ */
+var Comment = mongoose.model('Comment', CommentSchema);
+
+
+/**
  * Exports.
  */
 exports.Collection = Collection;
+exports.Comment = Comment;
